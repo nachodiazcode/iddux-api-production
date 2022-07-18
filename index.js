46ee09ddde23c44717278e1335143b47ba73b02c
ignacio.diaz <ignacio.diaz@grupomok.com>
2022-07-18 03:10:58 -0400

    cambio en el archivo vercel

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const app = require('./app')
 const log = require('./utils/logger')
 const config = require('./config/index')
+const express = require('express')
 
-const port = config.port ;
+require('dotenv').config()
 
-app.listen(config.puerto)
+const PUERTO = config.port || process.env.PORT ;
 
 
+const api = express();
+
+api.listen(PORT => console.log(`Corriendo en el puerto ${PUERTO}`) )
