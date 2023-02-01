d9d64185b660e514fc913f93922718be8ab1885b
ignaciodiaz <ignaciosergiodiaz@gmail.com>
2023-02-01 17:50:22 -0300

    cambiando puerto

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const ambiente = process.env.NODE_ENV || 'development'
 
 const configuracionBase = {
   jwt: 'esto_es_secreto',
-  puerto: 8080,
+  puerto: 'https://iddux.cl/api',
   s3: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_KEY
