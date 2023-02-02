80f02615e5d4f8ca800c6d05481c813d80d499e4
ignaciodiaz <ignaciosergiodiaz@gmail.com>
2023-02-01 23:44:12 -0300

    cambiando a puerto 3000

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwt: {
-      puerto:8080,
+      puerto:3000,
       secret: 'esto_es_secreto',
       tiempoDeExpiración: 24 * 60 * 60 
     }
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwt: {
-      puerto:8080,
+      puerto:3000,
       secret: 'esto_es_secreto',
       expiresIn: 24 * 60 * 60 
     }
